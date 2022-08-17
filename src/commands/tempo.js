766a3de1975b3cb4e41807110984aa6a82392d82
LucasVieiraS <lucas.silva3759@etec.sp.gov.br>
2022-08-16 21:45:02 -0300

    feat: weather command

---
+++ src/commands/tempo.js
@@ -0,0 +1,55 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("tempo")
+		.setDescription("o tempo atual de uma cidade aleatória")
+		.addStringOption((option) =>
+			option
+				.setName("cidade")
+				.setDescription("Temperatura na cidade citada")
+				.setRequired(true)
+		),
+	async execute(interaction) {
+                let city = interaction.options.getString("cidade") || "New York";
+                console.log(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHERTOKEN}`);
+                await axios
+                .get(
+                    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHERTOKEN}`
+                )
+                .then(response => {
+                    let apiData = response;
+                    let currentTemp = Math.ceil(apiData.data.main.temp)
+                    let maxTemp = apiData.data.main.temp_max;
+                    let minTemp = apiData.data.main.temp_min;
+                    let humidity = apiData.data.main.humidity;
+                    let wind = apiData.data.wind.speed;
+                    let icon = apiData.data.weather[0].icon
+                    let country = apiData.data.sys.country
+                    let pressure = apiData.data.main.pressure;
+                    let cloudness = apiData.data.weather[0].description;
+                    embed = new MessageEmbed()
+                    .setColor('#0099ff')
+                    .setTitle(`Está ${currentTemp}\u00B0 °C em ${city} - ${country}`)
+                    .setThumbnail(icon)
+                    .addFields(
+                                    [
+                                    {name: `Temperatura Máxima:`, value: `${maxTemp}\u00B0 C`},
+                                    {name: `Temperatura Mínima:`, value: `${minTemp}\u00B0 C`},
+                                    {name: `Umidade:`, value: `${humidity} %`},
+                                    {name: `Velocidade do Vento:`, value: `${wind} m/s`},
+                                    {name: `Pressão:`, value: `${pressure} hpa`},
+                                    {name: `Nebulosidade:`, value: `${cloudness}`}
+                                    ]
+                            )
+                    .setThumbnail(`http://openweathermap.org/img/w/${icon}.png`)
+                    interaction.reply({embeds: [embed]});
+                }).catch(err => {
+                    console.log(err);
+                    interaction.reply(`Adicione uma cidade válida.`)
+                })
+	}
+}
+ 
